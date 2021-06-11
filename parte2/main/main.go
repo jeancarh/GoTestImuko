a8230cf003e8620bd7278abe0bec86ebc260e07e
jean carlos hernandez <jchernandez@teravisiontech.com>
2021-06-11 10:41:24 -0500

    adding new part 2 completed

--- parte2/main/main.go
+++ parte2/main/main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"encoding/csv"
+	"encoding/json"
 	"fmt"
 	"os"
+	"sort"
+	"strings"
 )
 
 type Organization struct {
@@ -35,68 +38,32 @@ func main() {
 		rol  string
 	}
 	m := make(map[string][]string)
-	roles := []string{}
 	for _, line := range csvLines {
-		roles = append(roles, line[2])
-		m[line[0]+"-"+line[1]] = roles
-		roles = []string{}
+		m[line[0]+"-"+line[1]] = append(m[line[0]+"-"+line[1]], line[2])
 	}
 	validateData(m)
-
-	// final, _ := json.Marshal(users)
-	// fmt.Println(string(final))
 }
 
 func validateData(m map[string][]string) {
-	// roles := []string{}
-	// // userStack := []User{}
-	// // user := User{}
-	// // org := Organization{}
-	// for _, value := range m {
-	// 	res1 := strings.Split(value, "-")
-	// 	roles = append(roles, res1[2])
-	// 	m[res1[2]] =
-	// }
-	fmt.Println(m)
-
-}
-
-func unique(intSlice []string) []string {
-	keys := make(map[string]bool)
-	list := []string{}
-	for _, entry := range intSlice {
-		if _, value := keys[entry]; !value {
-			keys[entry] = true
-			list = append(list, entry)
+	var users []User
+	var orgs []Organization
+	for k, v := range m {
+		res1 := strings.Split(k, "-")
+		user := User{
+			Username: res1[1],
+			Roles:    v,
 		}
-	}
-	return list
-}
-
-func stringInSlice(usr string, list []string) bool {
-	for _, v := range list {
-		if v == usr {
-			return true
+		users = append(users, user)
+		org := Organization{
+			Organization: res1[0],
+			Users:        users,
 		}
+		orgs = append(orgs, org)
+		users = []User{}
 	}
-	return false
-}
-
-func stringInSliceRole(str string, list []string) bool {
-	for _, v := range list {
-		if v == str {
-			return true
-		}
-	}
-	return false
-}
-
-func stringInSliceOrg(org Organization, list []Organization) bool {
-	for _, v := range list {
-		if v.Organization == org.Organization {
-			fmt.Println(v.Organization + org.Organization)
-			return true
-		}
-	}
-	return false
+	sort.Slice(orgs, func(i, j int) bool {
+		return orgs[i].Organization < orgs[j].Organization
+	})
+	final, _ := json.Marshal(orgs)
+	fmt.Println(string(final))
 }
